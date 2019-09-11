87637279683abf02d7395670d6897f2e9b35f7e9
Hannah Tuttle <hannah@Hannahs-Air.PK5001Z>
2019-09-11 13:31:47 -0700

    added find steps function, tested and working

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -2,10 +2,29 @@ const db = require('../data/db-config.js')
 
 module.exports = {
     find,
+    findById,
+    findSteps
 }
 
 function find() {
     return db('schemes').then(schemes => {
         return schemes
+    })
+}
+
+function findById(id) {
+    return db('schemes').where('id', id)
+    .then(scheme => {
+        return scheme
+    })
+}
+
+function findSteps(id) {
+    return db('schemes as s')
+    .join('steps as st', 's.id', 'st.scheme_id')
+    .where('s.id', id)
+    .select('st.id','s.scheme_name', 'st.step_number', 'st.instructions')
+    .then(steps => {
+        return steps
     })
 }
